e9a66659188889b7125cf88078de9e92e5df4416
nalbam <me@nalbam.com>
2020-02-20 20:51:42 +0900

    get_pressed

--- cam.py
+++ cam.py
@@ -30,42 +30,6 @@ MAXTEMP = 32.0
 # how many color values we can have
 COLORDEPTH = 1024
 
-os.putenv("SDL_FBDEV", "/dev/fb1")
-
-pygame.init()
-
-# initialize the sensor
-sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
-
-# pylint: disable=invalid-slice-index
-points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
-grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
-# pylint: enable=invalid-slice-index
-
-# sensor is an 8x8 grid so lets do a square
-height = 240
-width = 240
-
-# the list of colors we can choose from
-blue = Color("indigo")
-colors = list(blue.range_to(Color("red"), COLORDEPTH))
-
-# create the array of colors
-colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
-
-displayPixelWidth = width / 30
-displayPixelHeight = height / 30
-
-lcd = pygame.display.set_mode((width, height))
-
-lcd.fill((255, 0, 0))
-
-pygame.display.update()
-pygame.mouse.set_visible(False)
-
-lcd.fill((0, 0, 0))
-
-pygame.display.update()
 
 # some utility functions
 def constrain(val, min_val, max_val):
@@ -76,31 +40,86 @@ def map_value(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-# let the sensor initialize
-time.sleep(0.1)
+def run():
+    os.putenv("SDL_FBDEV", "/dev/fb1")
 
-while True:
-    # read the pixels
-    pixels = []
-    for row in sensor.pixels:
-        pixels = pixels + row
-    pixels = [map_value(p, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1) for p in pixels]
+    pygame.init()
 
-    # perform interpolation
-    bicubic = griddata(points, pixels, (grid_x, grid_y), method="cubic")
+    # initialize the sensor
+    sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
 
-    # draw everything
-    for ix, row in enumerate(bicubic):
-        for jx, pixel in enumerate(row):
-            pygame.draw.rect(
-                lcd,
-                colors[constrain(int(pixel), 0, COLORDEPTH - 1)],
-                (
-                    displayPixelHeight * ix,
-                    displayPixelWidth * jx,
-                    displayPixelHeight,
-                    displayPixelWidth,
-                ),
-            )
+    # pylint: disable=invalid-slice-index
+    points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
+    grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+    # pylint: enable=invalid-slice-index
 
+    # sensor is an 8x8 grid so lets do a square
+    height = 240
+    width = 240
+
+    # the list of colors we can choose from
+    blue = Color("indigo")
+    colors = list(blue.range_to(Color("red"), COLORDEPTH))
+
+    # create the array of colors
+    colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
+
+    displayPixelWidth = width / 30
+    displayPixelHeight = height / 30
+
+    lcd = pygame.display.set_mode((width, height))
+
+    lcd.fill((255, 0, 0))
     pygame.display.update()
+
+    pygame.mouse.set_visible(False)
+
+    lcd.fill((0, 0, 0))
+    pygame.display.update()
+
+    # let the sensor initialize
+    time.sleep(0.1)
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
+            run = False
+
+        if run == False:
+            break
+
+        # read the pixels
+        pixels = []
+        for row in sensor.pixels:
+            pixels = pixels + row
+        pixels = [map_value(p, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1) for p in pixels]
+
+        # perform interpolation
+        bicubic = griddata(points, pixels, (grid_x, grid_y), method="cubic")
+
+        # draw everything
+        for ix, row in enumerate(bicubic):
+            for jx, pixel in enumerate(row):
+                pygame.draw.rect(
+                    lcd,
+                    colors[constrain(int(pixel), 0, COLORDEPTH - 1)],
+                    (
+                        displayPixelHeight * ix,
+                        displayPixelWidth * jx,
+                        displayPixelHeight,
+                        displayPixelWidth,
+                    ),
+                )
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
+run()
