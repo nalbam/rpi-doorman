719076db19767b59d6ebc28fa246c26326fff674
nalbam <me@nalbam.com>
2020-02-24 14:36:52 +0900

    23 / 31

--- run.py
+++ run.py
@@ -18,10 +18,10 @@ from botocore.session import Session
 
 
 # low range of the sensor (this will be blue on the screen)
-MINTEMP = 24.0
+MINTEMP = 23.0
 
 # high range of the sensor (this will be red on the screen)
-MAXTEMP = 32.0
+MAXTEMP = 31.0
 
 # how many color values we can have
 COLORDEPTH = 1024
