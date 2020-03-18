449dc2207c78bb401b3ba4e61bb4a175efdf864c
nalbam <me@nalbam.com>
2020-03-18 21:24:14 +0900

    save_json

--- lepton.py
+++ lepton.py
@@ -5,6 +5,7 @@
   It will not work on microcontrollers running CircuitPython!
 """
 
+import argparse
 import cv2
 import math
 import numpy as np
@@ -17,6 +18,9 @@ from pylepton.Lepton3 import Lepton3
 
 from colormap import colormap
 
+from pathlib import Path
+
+HOME = str(Path.home())
 
 # low range of the sensor
 MINTEMP = 29000
@@ -25,6 +29,37 @@ MINTEMP = 29000
 MAXTEMP = 31000
 
 FRAME_RATE = 15
+
+JSON_PATH = os.environ.get("JSON_PATH", "{}/.doorman.json".format(HOME))
+
+
+def parse_args():
+    p = argparse.ArgumentParser(description="doorman")
+    p.add_argument("--device", default="/dev/spidev0.0", help="device")
+    p.add_argument("--width", type=int, default=160, help="width")
+    p.add_argument("--height", type=int, default=120, help="height")
+    p.add_argument("--pixel", type=int, default=3, help="pixel")
+    p.add_argument("--json-path", default=JSON_PATH, help="json path")
+    return p.parse_args()
+
+
+def load_json(json_path=JSON_PATH):
+    if os.path.isfile(json_path):
+        f = open(json_path)
+        data = json.load(f)
+        f.close()
+    else:
+        data = {"filename": "", "uploaded": False}
+        save_json(json_path, data)
+    return data
+
+
+def save_json(json_path=JSON_PATH, data=None):
+    if data == None:
+        data = {"filename": "", "uploaded": False}
+    with open(json_path, "w") as f:
+        json.dump(data, f)
+    f.close()
 
 
 def get_color(v):
@@ -36,17 +71,18 @@ def get_color(v):
     )
 
 
-def run():
-    device = "/dev/spidev0.0"
+def main():
+    args = parse_args()
 
-    width = 160
-    height = 120
+    device = args.device
+
+    width = args.width
+    height = args.height
 
     pixels = np.zeros((height, width, 1), dtype=np.uint16)
-    # length = width * height
 
-    pixel_width = 3
-    pixel_height = 3
+    pixel_width = args.pixel
+    pixel_height = args.pixel
 
     screen_width = width * pixel_width
     screen_height = height * pixel_height
@@ -54,7 +90,7 @@ def run():
     # pygame
     pygame.init()
 
-    # clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
     screen = pygame.display.set_mode((screen_width, screen_height))
 
@@ -109,10 +145,16 @@ def run():
                     ),
                 )
 
+        if max_temp > MAXTEMP:
+            filename = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M%S-%f")
+            data = {"filename": filename, "uploaded": False}
+            save_json(args.json_path, data)
+
         pygame.display.update()
-        # clock.tick(FRAME_RATE)
+        clock.tick(FRAME_RATE)
 
     pygame.quit()
 
 
-run()
+if __name__ == "__main__":
+    main()
