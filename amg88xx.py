869f174a11e35335873ff15547d2c34fe4c685fe
nalbam <me@nalbam.com>
2020-03-19 11:30:12 +0900

    datetime

--- amg88xx.py
+++ amg88xx.py
@@ -6,6 +6,7 @@
 """
 
 import argparse
+import datetime
 import math
 import numpy as np
 import os
--- lepton.py
+++ lepton.py
@@ -7,6 +7,7 @@
 
 import argparse
 import cv2
+import datetime
 import math
 import numpy as np
 import os
