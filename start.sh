91ce5394d2a64998770741413dccfa0a5fa18dd3
nalbam <me@nalbam.com>
2020-03-24 17:44:24 +0900

    nohup

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./LeptonModule/software/raspberrypi_video/raspberrypi_video -mirror -min 29000 -max 31200
+nohup ./LeptonModule/software/raspberrypi_video/raspberrypi_video -mirror -min 29000 -max 31200 &
 
-./rpi-doorman/camera.py
+nohup ./rpi-doorman/camera.py &
