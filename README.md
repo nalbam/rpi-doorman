c8f7b2f20247e7a65502a81c053d4c52119bd8d9
nalbam <me@nalbam.com>
2020-06-19 15:27:10 +0900

    setup

--- README.md
+++ README.md
@@ -14,23 +14,7 @@ Interfacing Options -> I2C -> Enabled
 ## Install Python Software
 
 ```bash
-# sudo apt install -y python3-scipy python3-pygame
-
-pip3 install awscli
-pip3 install boto3
-pip3 install colour
-pip3 install opencv-python
-pip3 install pygame
-pip3 install scipy
-
-pip3 install adafruit-blinka
-pip3 install adafruit-circuitpython-amg88xx
-```
-
-### for Windows in vs-code terminal
-
-```bash
-py3 -m pip install boto3
+./setup.sh
 ```
 
 ## Test
@@ -48,6 +32,3 @@ python3 ./rpi-doorman/camera.py --bucket-name ${BUCKET_NAME}
 
 python3 ./rpi-doorman/amg88xx.py --min 18 --max 26
 ```
-
-* <https://learn.adafruit.com/adafruit-amg8833-8x8-thermal-camera-sensor/python-circuitpython>
-* <https://github.com/adafruit/Adafruit_CircuitPython_AMG88xx>
--- setup.sh
+++ setup.sh
@@ -6,12 +6,15 @@ sudo apt upgrade -y
 sudo apt install -y build-essential cmake pkg-config qt5-default gfortran \
                     libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev \
                     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
-                    libxvidcore-dev libx264-dev libgtk2.0-dev libgtk-3-dev \
-                    libatlas-base-dev
+                    libxvidcore-dev libx264-dev libatlas-base-dev libhdf5-dev
 
-sudo apt install -y python2.7-dev python3-dev
+# sudo apt install -y libgtk2.0-dev libgtk-3-dev libjasper1
 
-sudo apt install -y python3-scipy python3-pygame
+# sudo apt install -y python2.7-dev
+
+sudo apt install -y python3-dev python3-scipy python3-pygame python3-pyqt5
+
+sudo apt install -y qt4-dev-tools
 
 sudo apt autoremove -y
 
@@ -19,14 +22,16 @@ pip3 install boto3
 pip3 install cmake
 pip3 install colour
 pip3 install cython
-pip3 install opencv-python==3.4.6.27
 pip3 install scipy
+pip3 install imutils
 
-pip3 install adafruit-blinka
-pip3 install adafruit-circuitpython-amg88xx
+# pip3 install adafruit-blinka
+# pip3 install adafruit-circuitpython-amg88xx
 
 # pip3 install face_recognition
-# pip3 install imutils
+
+pip3 install opencv-python==3.4.6.27
+
 # pip3 install opencv-python-headless
 # pip3 install opencv-contrib-python
 
